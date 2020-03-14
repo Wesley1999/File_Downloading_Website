438af350c1324f2eb71bbbabd5dfcee4a812c027
Wesley1999 <a13207123727@gmail.com>
2020-03-14 13:50:35 +0800

    补充

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+
+    layui.use(['element', 'layer']);
+
+    $(".layui-btn").click(function () {
+        location.href = $(this).next().text();
+    });
+
+    $(".url").dblclick(function () {
+        let url = $(this).text();
+        if (copyText(url)) {
+            toast(url+"\n已复制到剪切板", 1000)
+        }
+    });
+
+
+    // ----------------------------------
+
+    function toast(msg, time=2000, parse=false) {
+        if (!parse) {
+            msg = msg.replaceAll("&", "&amp;");
+            msg = msg.replaceAll(">", "&gt;");
+            msg = msg.replaceAll("<", "&lt;")
+        }
+        msg = msg.replaceAll("\n", "<br>");
+        layer.msg(msg, {
+            time: time
+        });
+
+    }
+
+    function copyText(text) {
+        let flag = false;
+        let textarea = document.createElement("input");
+        textarea.style.position = 'absolute';
+        textarea.style.top = '50%';
+        textarea.style.left = '50%';
+        let currentFocus = document.activeElement;
+        document.body.appendChild(textarea);
+        textarea.value = text;
+        textarea.focus();
+        if (textarea.setSelectionRange)
+            textarea.setSelectionRange(0, textarea.value.length);
+        else
+            textarea.select();
+        try {
+            flag = document.execCommand("copy");
+        } catch (eo) {}
+        document.body.removeChild(textarea);
+        currentFocus.focus();
+        return flag;
+    }
+
+    String.prototype.replaceAll = function(s1,s2){
+        return this.replace(new RegExp(s1,"gm"),s2);
+    };
+
+});
+
+
+
